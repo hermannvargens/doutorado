035f1ad772ff55a407383131fafdc486487a9353
Hermann Vargens <96803586+hermannvargens@users.noreply.github.com>
2025-03-26 22:47:03 -0300

    Update analiseNIR.py

--- analiseNIR.py
+++ analiseNIR.py
@@ -3,36 +3,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Carregar os dados (Substitua pelo carregamento adequado do seu dataset)
-@st.cache_data
-def load_data():
-    # Simulação de um dataset (Substituir pelo real)
-    df = pd.read_csv("espectros_derivada.csv")
-    return df
-
-df = load_data()
-
-# Dividir o df, tomando da 4ª coluna em diante, onde estão os valores de Absorbance
-df_espectros = df.iloc[:, 3:]
-
-# Transpor os dados
-df_espectros = df_espectros.T
-
-# Criar interface
+# Carregar os dados
 st.title("Visualização de Espectros NIR")
-st.write("Selecione os espectros que deseja visualizar:")
 
-# Selecionar amostras específicas
-samples = st.multiselect("Amostras", df_espectros.columns, default=df_espectros.columns[:5])
+uploaded_file = st.file_uploader("Faça o upload do arquivo CSV", type=["csv"])
 
-# Plot
-fig, ax = plt.subplots(figsize=(10, 5))
-for column in samples:
-    ax.plot(df_espectros.index, df_espectros[column], label=f"Amostra {column}")
-
-ax.set_xlabel("Wavelength")
-ax.set_ylabel("Absorbance")
-ax.grid(True)
-ax.set_xticks(np.arange(0, len(df_espectros.index), 10))
-ax.legend()
-st.pyplot(fig)
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file, delimiter=";")
+    df = df[:-3]
+    
+    # Substituir '.' por '' e ',' por '.' nos nomes das colunas
+    df.columns = [col.replace('.', '').replace(',', '.') for col in df.columns]
+    
+    # Substituir o ponto por '' e a vírgula por ponto em todas as colunas do df
+    for col in df.columns:
+        if df[col].dtype == 'object':  # Verificar se a coluna contém strings
+            df[col] = df[col].str.replace('.', '', regex=False).str.replace(',', '.', regex=False)
+    
+    # Converter todas as colunas para float
+    df = df.astype(float)
+    
+    # Separar os valores de absorbância
+    df_espectros = df.iloc[:, 3:]
+    df_espectros = df_espectros.T  # Transpor os dados
+    
+    # Criar o gráfico
+    st.subheader("Espectros NIR")
+    fig, ax = plt.subplots(figsize=(10, 5))
+    
+    for column in df_espectros.columns:
+        ax.plot(df_espectros.index, df_espectros[column], label=column)
+    
+    ax.set_xlabel("Comprimento de Onda")
+    ax.set_ylabel("Absorbância")
+    ax.grid(True)
+    ax.set_xticks(np.arange(0, len(df_espectros.index), 10))
+    
+    st.pyplot(fig)
