3d968807ba2c1bfb8975372b3bc287e1f216e8aa
Hermann Vargens <96803586+hermannvargens@users.noreply.github.com>
2025-04-05 21:17:14 -0300

    Update analiseNIR.py

--- analiseNIR.py
+++ analiseNIR.py
@@ -31,7 +31,7 @@ if uploaded_file is not None:
 
     # Passo 3: Selecione as variáveis de entrada (X) e, se disponível, o alvo (y)
     # Supondo que o arquivo tenha colunas para predição e as variáveis X de forma geral:
-    X = df  # Aqui, estamos assumindo que todas as colunas são variáveis independentes para a previsão.
+    X = df.values  # Aqui, estamos assumindo que todas as colunas são variáveis independentes para a previsão.
 
     # Passo 4: Fazer previsões com o modelo
     y_pred = pls.predict(X)
